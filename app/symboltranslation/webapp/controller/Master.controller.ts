82025161b30b3a8672a1e852d96bfa4e89ba37bd
AlbertoCosmaPXL <alberto.cosma@student.pxl.be>
2025-11-12 13:09:58 +0100

    Symbol translation working as intended!

--- app/symboltranslation/webapp/controller/Master.controller.ts
+++ app/symboltranslation/webapp/controller/Master.controller.ts
@@ -54,6 +54,7 @@ export default class Master extends Controller {
       symbol: data?.getProperty("/symbol") as string,
       whereFound: data?.getProperty("/whereFound") as string,
       language: data?.getProperty("/language") as string,
+      translation: data?.getProperty("/translation") as string,
     });
 
     this.creationDialog.close();
